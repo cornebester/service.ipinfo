932f1c7aa00862d3e95e1cd66ad2d5fbbd33ef46
Corné Bester <corne@quanthealth.ai>
2026-04-22 20:08:09 +0200

    fix: tf resource name

--- infra/eks-admin-me.tf
+++ infra/eks-admin-me.tf
@@ -5,7 +5,7 @@ resource "aws_eks_access_entry" "caller" {
   type = "STANDARD"
 }
 
-resource "aws_eks_access_policy_association" "corne_AmazonEKSAdminPolicy" {
+resource "aws_eks_access_policy_association" "caller_AmazonEKSAdminPolicy" {
   cluster_name  = aws_eks_cluster.eks_lab.name
   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSAdminPolicy"
   principal_arn = data.aws_iam_session_context.current.issuer_arn
@@ -16,7 +16,7 @@ resource "aws_eks_access_policy_association" "corne_AmazonEKSAdminPolicy" {
   }
 }
 
-resource "aws_eks_access_policy_association" "corne_AmazonEKSClusterAdminPolicy" {
+resource "aws_eks_access_policy_association" "caller_AmazonEKSClusterAdminPolicy" {
   cluster_name  = aws_eks_cluster.eks_lab.name
   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
   principal_arn = data.aws_iam_session_context.current.issuer_arn
